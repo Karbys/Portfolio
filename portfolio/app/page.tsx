bc977b90031f6985e71d406eec82eae23d489cc7
Emma Paris <Bewantza33@gmail.com>
2025-10-11 15:43:50 +0700

    delete all

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,21 +1,8 @@
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Skills from '../components/Skills';
-import Projects from '../components/Projects';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </main>
+    <>
+    </>
   );
 }
